2a2bcfb855109694e8fb763312d8e8bf471ddaa7
Suriyaprakash99 <49790387+Suriyaprakash99@users.noreply.github.com>
2019-06-16 00:36:03 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -5,13 +5,8 @@ int main()
 {
 int a;
 scanf("%d",&a);
-if(a%400==0)
-printf("yes");
-else if(a%100==0)
-  printf("no");
-else if(a%4==0)
-printf("yes");
-else
-  printf("no");
+for(int i=1;i<=a;i++)
+{
+  printf("\nHello");
 return 0;
 }

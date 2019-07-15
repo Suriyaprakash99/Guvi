da288148e056bf794e149de9f75b90fd528cbea2
Suriyaprakash99 <49790387+Suriyaprakash99@users.noreply.github.com>
2019-07-15 09:49:51 +0530

    Update pro1_repeatno.py

--- pro1_repeatno.py
+++ pro1_repeatno.py
@@ -1,13 +1,10 @@
 a=input("")
 a1=list(a.split(" "))
-#print(a1)
 b=[]
 for i in range(0,len(a1)):
   for j in range(i+1,len(a1)):
     if(a1[i]==a1[j]):
-      #print(a1[i])
       if a1[i] not in b:
         b.append(a1[i])
-#print(b)
 c=" ".join(b)
 print(c)
